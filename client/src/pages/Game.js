8bc82d698bd5d0dcf0804e790b7e10c2e087d8e4
Aaron Kau <aaron.kau@me.com>
2021-05-11 22:33:37 -0700

    clean up some code and fix socket connection

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,17 +1,10 @@
 import React, { Component } from "react";
 import { useParams } from "react-router-dom";
-import { io } from "socket.io-client";
+import io from "socket.io-client";
 import PlayerCard from "../components/PlayerCard";
 import ChooseName from "../components/ChooseName";
 import Lobby from "../components/Lobby";
 
-/*
-import { LeftBrain, RightBrain } from "../components/Shapes";
-import DirectionToggle from "../components/DirectionToggle";
-import Clue from "../components/Clue";
-import Target from "../components/Target";
-import ScoreIndicator from "../components/ScoreIndicator";
-*/
 import { Token, Role, Item, Step } from "../enums";
 
 export default function Game() {
@@ -27,218 +20,38 @@ class Device extends Component {
     this.state = {
       playerName: "",
       gameState: {},
-      psychic: false,
-      controlsDisabled: false,
-      client: null,
-      roundNum: 0,
-      gameId: 0,
-      loading: true,
+      loading: true
     };
-  }
-
-  disableControls() {
-    this.setState({
-      controlsDisabled: true,
-    });
-    window.setTimeout(() => {
-      this.setState({
-        controlsDisabled: false,
-      });
-    }, 1010);
   }
 
   componentDidMount() {
     const serverUrl = process.env.NODE_ENV === "production" ? "/" : ":8080/";
-    console.log(serverUrl);
     const client = io(serverUrl);
     const { lobbyId } = this.props;
-    console.log("Component Mounted");
     client.on("connect", (socket) => {
       client.emit("requestGameState", lobbyId);
-      console.log("REQUEST GAME STATE " + lobbyId);
     });
     client.on("gameState", (updatedState) => {
-      const gameState = this.state.gameState;
-
-      // If we update target position right away it reveals the result...
-      const targetPosition = updatedState.targetPosition;
-      if (!this.state.loading) {
-        delete updatedState.targetPosition;
-      }
-
-      // Reset peek on new round
-      let psychic = this.state.psychic;
-      if (
-        updatedState.roundNum !== gameState.roundNum ||
-        updatedState.gameId !== gameState.gameId
-      ) {
-        psychic = false;
-        this.disableControls();
-      }
-
-      this.setState({
-        gameState: { ...gameState, ...updatedState },
-        psychic: psychic,
-        loading: false,
-      });
-
-      window.setTimeout(() => {
-        const gameState = this.state.gameState;
-        this.setState({
-          gameState: { ...gameState, targetPosition: targetPosition },
-        });
-      }, 1010);
+      const { gameState } = this.state;
+      this.setState({ loading: false, gameState: { ...gameState, ...updatedState }});
+      console.log(`UPDATED GAME STATE: ${updatedState}`)
     });
-    this.setState({
-      client: client,
-    });
+    this.client = client;
   }
+
   setPlayerName = (playerName) => {
-    const { client, lobbyId } = this.state;
+    const { lobbyId } = this.state;
     this.setState({
       playerName: playerName
     });
-    client.emit("joinGame", lobbyId, playerName);
+    this.client.emit("joinGame", lobbyId, playerName);
   }
   playerCardClicked = (event) => {
     console.log("Hello at PlayerCard");
-    /*
-    const { client, gameState, psychic, controlsDisabled } = this.state;
-    const { lobbyId } = this.props;
-
-    if (!gameState.screenClosed || psychic || controlsDisabled) {
-      return;
-    }
-
-    this.setState({
-      gameState: {
-        ...gameState,
-        direction: Direction.getOther(gameState.direction),
-        },
-      },
-      () => {
-        client.emit("setDirection", lobbyId, this.state.gameState.direction);
-    });
-    */
-  };
-  // togglePsychicClicked = (event) => {
-  //   const { psychic, controlsDisabled } = this.state;
-
-  //   if (controlsDisabled) {
-  //     return;
-  //   }
-
-  //   this.setState({
-  //     psychic: !psychic,
-  //   });
-  // };
-
-  // directionToggleClicked = (event) => {
-  //   const { client, gameState, psychic, controlsDisabled } = this.state;
-  //   const { lobbyId } = this.props;
-
-  //   if (!gameState.screenClosed || psychic || controlsDisabled) {
-  //     return;
-  //   }
-
-  //   this.setState(
-  //     {
-  //       gameState: {
-  //         ...gameState,
-  //         direction: Direction.getOther(gameState.direction),
-  //       },
-  //     },
-  //     () => {
-  //       client.emit("setDirection", lobbyId, this.state.gameState.direction);
-  //     }
-  //   );
-  // };
-
-  // newGameClicked = (event) => {
-  //   let { client, controlsDisabled } = this.state;
-  //   const { lobbyId } = this.props;
-
-  //   if (controlsDisabled) {
-  //     return;
-  //   }
-
-  //   this.disableControls();
-  //   client.emit("newGame", lobbyId);
-  // };
-
-  // dialClicked = (event) => {
-  //   const { client, gameState, psychic, controlsDisabled } = this.state;
-  //   const { lobbyId } = this.props;
-  //   if (!gameState.screenClosed || psychic || controlsDisabled) {
-  //     return;
-  //   }
-
-  //   const rect = this.deviceInner.current.getBoundingClientRect();
-
-  //   const xOrigin = rect.x + rect.width / 2;
-  //   const yOrigin = rect.y + rect.height;
-
-  //   const xOffset = event.clientX - xOrigin;
-  //   const yOffset = event.clientY - yOrigin;
-
-  //   let rotation = 2 * Math.PI - Math.atan2(xOffset, yOffset);
-  //   if (rotation > 2 * Math.PI) {
-  //     rotation -= 2 * Math.PI;
-  //   }
-
-  //   let dialPosition = rotation / Math.PI - 0.5;
-  //   dialPosition = Math.min(dialPosition, 0.95);
-  //   dialPosition = Math.max(dialPosition, 0.05);
-
-  //   this.setState(
-  //     {
-  //       gameState: {
-  //         ...gameState,
-  //         dialPosition: dialPosition,
-  //       },
-  //     },
-  //     () => {
-  //       client.emit(
-  //         "setDialPosition",
-  //         lobbyId,
-  //         this.state.gameState.dialPosition
-  //       );
-  //     }
-  //   );
-  // };
-
-  screenHandleClicked = (event) => {
-    let { client, gameState, controlsDisabled } = this.state;
-    let { screenClosed, targetPosition } = gameState;
-    const { lobbyId } = this.props;
-
-    if (controlsDisabled) {
-      return;
-    }
-
-    this.setState(
-      {
-        gameState: {
-          ...gameState,
-          screenClosed: !screenClosed,
-          targetPosition: targetPosition,
-        },
-      },
-      () => {
-        if (screenClosed) {
-          client.emit("reveal", lobbyId);
-        } else {
-          this.setState({
-            psychic: false,
-          });
-          client.emit("nextRound", lobbyId);
-        }
-      }
-    );
   };
 
   render() {
-    const { psychic, gameState, loading, playerName } = this.state;
+    const { gameState, playerName, loading } = this.state;
 
     const elements = ['test', 'test2'];
     if (loading) {
@@ -336,90 +149,9 @@ class Device extends Component {
         <p>Game Over</p>
         </div>
       )
+    } else {
+      throw Error(`Unable to render! Unknown step: ${step}.`);
     }
-    // const rotation = Math.PI * (dialPosition + 1.5);
-
-    /*
-    const winner =
-      score[Team.LEFT_BRAIN] <= score[Team.RIGHT_BRAIN]
-        ? Team.RIGHT_BRAIN
-        : Team.LEFT_BRAIN;
-
-    return (
-      <div className="device_parent">
-        <div className="device_outer">
-          <div className="device">
-            <div className="device_red" />
-            <div className="device_wing_l" />
-            <div className="device_wing_r" />
-
-            <div
-              className="device_inner"
-              ref={this.deviceInner}
-              onMouseDown={this.dialClicked}
-            >
-              <div
-                className="dial"
-                style={{ transform: `rotate(${rotation}rad)` }}
-              />
-              <Target targetPosition={targetPosition} />
-              <div
-                className={(psychic ? "peek" : "") + " screen"}
-                style={{ transform: `rotate(${screenClosed ? 360 : 187}deg)` }}
-              />
-            </div>
-
-            <div
-              className="screenHandle"
-              onMouseDown={
-                complete ? this.newGameClicked : this.screenHandleClicked
-              }
-              style={{
-                transform: `rotate(${screenClosed ? 356.5 : 183.5}deg)`,
-              }}
-            >
-              <div
-                className={
-                  (screenClosed ? "" : "flipped") + " screenHandleText"
-                }
-              >
-                <span>{screenClosed ? "Reveal" : "Next Round"}</span>
-              </div>
-            </div>
-          </div>
-          <div className="togglePeek" onMouseDown={this.togglePsychicClicked}>
-            Psychic
-          </div>
-          <DirectionToggle
-            direction={direction}
-            onMouseDown={this.directionToggleClicked}
-            psychic={psychic}
-          />
-          <Clue clues={clues} color={clueColor} />
-          <ScoreIndicator
-            isTurn={turn === Team.LEFT_BRAIN}
-            team={Team.LEFT_BRAIN}
-            score={score[Team.LEFT_BRAIN]}
-          />
-          <ScoreIndicator
-            isTurn={turn === Team.RIGHT_BRAIN}
-            team={Team.RIGHT_BRAIN}
-            score={score[Team.RIGHT_BRAIN]}
-          />
-          {complete ? (
-            <div className="gameOver">
-              {winner == Team.LEFT_BRAIN ? <LeftBrain /> : <RightBrain />}
-              Team {winner ? "Right" : "Left"} Brain wins!
-              <div className="newGame" onMouseDown={this.newGameClicked}>
-                New Game
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
-      </div>
-    );*/
-  }    
+  }
 
 }
