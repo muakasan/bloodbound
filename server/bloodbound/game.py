00b254695aaa1f825347f782e92c23c86e7e204c
warut-vijit <warut.vijitbenjaronk@gmail.com>
2021-05-16 03:28:16 +0000

    first stab (pun intended) at game fsm

--- server/bloodbound/game.py
+++ server/bloodbound/game.py
@@ -1,22 +1,10 @@
 from typing import Optional
-from bloodbound.game_state import PlayerID, GameState, Team, Step, Token
+#from bloodbound.game_state import PlayerID, GameState, Item, Role, Team, Step, Token
+from game_state import PlayerID, GameState, Item, Role, Team, Step, Token
 from flask_socketio import SocketIO, join_room
 import random
 
 GameCode = str
-
-# Mock SocketIO for unit testing
-if False:
-    class SocketIO:
-        def __init__(self):
-            self.rooms = {}
-
-        def emit(self, msg, data, json, room):
-            print(f'{msg}@{room}(JSON:{json}): {data}')
-
-    def join_room(game_code: GameCode):
-        print(f'joined room {game_code}')
-
 
 class Game:
     def handler(*steps: Step):
@@ -32,17 +20,33 @@ class Game:
                     if _type is PlayerID and arg not in self.player_ids:
                         print(f'Arg {name} pid {arg}, expected {self.player_ids}')
                         valid = False
-                res = func(*args) if valid else None
+                res = func(self, *args) if valid else None
                 self.emit_game_state()
                 return res
             return _wrap
         return _dec
 
+    def _wound_helper(self, pid: PlayerID, token: Token):
+        player = self.state.players[pid]
+        if token not in player.tokens:
+            return False
+        player.shown_tokens.append(token)
+        #TODO: special case for inquisitor tokens
+        player.tokens.remove(token)
+        if len(player.shown_tokens) == 3: 
+            for sword, shield in [(Item.SWORD_I, Item.SHIELD_I), (Item.SWORD_II, Item.SHIELD_II)]:
+                if sword in player.items:
+                    player.items.remove(sword)
+                    for p in self.player_ids:
+                        if shield in self.state.players[p].items:
+                            self.state.players[p].items.remove(shield)
+        return True
+
     def __init__(self, sio: SocketIO, game_code: GameCode):
         self.sio = sio
         self.game_code = game_code
         self.player_ids = set()
-        self.new_game()
+        self.state = GameState()
 
     def request_game_state(self) -> None:
         join_room(self.game_code)
@@ -51,82 +55,258 @@ class Game:
     def emit_game_state(self) -> None:
         self.sio.emit("gameState", self.state.encode(), json=True, room=self.game_code)
 
-    def new_game(self):
-        self.state = GameState.build(self.player_ids, dummy=True)
-
     def join_game(self, pid: PlayerID) -> None:
         if self.state.step == Step.LOBBY:
             join_room(self.game_code)
             self.player_ids.add(pid)
         self.emit_game_state()
 
+    @handler(Step.LOBBY)
+    def start_game(self, pid: PlayerID) -> None:
+        self.state.reset(self.player_ids)
+        self.state.step = Step.ACK_PEEK
+
+    @handler(Step.ACK_PEEK)
+    def ack_peek(self, pid: PlayerID) -> None:
+        self.state.peeked[pid] = True
+        if set(self.state.peeked.keys()) == set(self.player_ids):
+            self.state.active = random.choice(list(self.player_ids))
+            self.state.step = Step.SET_TARGET
+
     # Stabbing
     @handler(Step.SET_TARGET)
     def set_target(self, pid: PlayerID, target_pid: PlayerID) -> None:
-        if pid == self.state.active and pid != target_pid:
+        target = self.state.players[target_pid]
+        if pid == self.state.active and pid != target_pid and \
+                (Item.SHIELD_I not in target.items) and \
+                (Item.SHIELD_II not in target.items):
             self.state.target = target_pid
-            self.state.step = Step.INTERVENE
-            self.state.intervene_offers = {target_pid: False}
+            if Item.FAN in target.items and len(player.shown_tokens) == 3:
+                self.state.captured = target_pid
+                self.state.step = Step.COMPLETE
+            elif Item.FAN in target.items:
+                self.state.intervener = None
+                self.state.wounded = target_pid
+                self.state.step = Step.SET_WOUND_I
+            else:
+                # the target and players showing rank cannot intervene
+                ineligible_players = [target_pid] + [p for p in self.player_ids if \
+                        Token.RANK in self.state.players[p].shown_tokens]
+                self.state.intervene_offers = {p: False for p in ineligible_players}
+                self.state.step = Step.INTERVENE
 
     @handler(Step.INTERVENE)
     def intervene(self, pid: PlayerID, offer: bool) -> None:
-        if pid != self.state.target:
-            self.intervene_offers[pid] = offer
+        player = self.state.players[pid]
+        if pid != self.state.target and \
+                (Token.RANK not in player.shown_tokens):
+            self.state.intervene_offers[pid] = offer
         if set(self.state.intervene_offers.keys()) == set(self.player_ids):
             self.state.step = Step.ACK_INTERVENE
 
     @handler(Step.ACK_INTERVENE)
     def ack_intervene(self, pid: PlayerID, offer_pid: Optional[PlayerID]) -> None:
+        player = self.state.players[pid]
         if pid == self.state.target:
-            if offer_pid is None:
+            if offer_pid is None and len(player.shown_tokens) == 3:
+                self.state.captured = pid
+                self.state.step = Step.COMPLETE
+            elif offer_pid is None:
                 self.state.intervener = None
+                self.state.wounded = pid
                 self.state.step = Step.SET_WOUND_I
             elif offer_pid != pid and self.state.intervene_offers[offer_pid]:
                 self.state.intervener = offer_pid
-                self.state.step = Step.SET_WOUND_I
+                self.state.wounded = offer_pid
+                self.state.step = Step.SET_ABILITY
 
-    # Can use ability (has not yet taken rank token)
+    # No intervention
     @handler(Step.SET_WOUND_I)
     def set_wound_i(self, pid: PlayerID, token: Token):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.target and self._wound_helper(pid, token):
+            if token == Token.RANK:
+                self.state.step = Step.SET_ABILITY
+            else:
+                self.state.active = pid
+                self.state.step = Step.SET_TARGET
 
     @handler(Step.SET_ABILITY)
     def no_ability(self, pid: PlayerID):
-        raise NotImplementedError
+        if pid == self.state.wounded:
+            self.state.active = pid
+            self.state.step = Step.SET_TARGET
 
     @handler(Step.SET_ABILITY)
     def elder_ability(self, pid: PlayerID):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.wounded and player.role == Role.ELDER:
+            player.items.append(Item.QUILL)
+            self.state.active = pid
+            self.state.step = Step.SET_TARGET
 
     @handler(Step.SET_ABILITY)
     def assassin_ability(self, pid: PlayerID, target_pid: PlayerID):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.wounded and player.role == Role.ASSASSIN:
+            target = self.state.players[target_pid]
+            if len(target.shown_tokens) >= 2:
+                self.state.captured = target_pid
+                self.state.step = Step.COMPLETE
+            else:
+                self.state.wounded = target_pid
+                self.state.step = Step.ASSASSIN_WOUND
+
+    @handler(Step.ASSASSIN_WOUND)
+    def assassin_wound(self, pid: PlayerID, token_1: Token, token_2: Token):
+        player = self.state.players[pid]
+        if pid == self.state.wounded:
+            if self._wound_helper(pid, token_1) and \
+                    self._wound_helper(pid, token_2):
+                self.state.active = pid
+                self.state.step = Step.SET_TARGET
 
     @handler(Step.SET_ABILITY)
     def harlequin_ability(self, pid: PlayerID, target_pid_1: PlayerID, target_pid_2: PlayerID):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.wounded and player.role == Role.HARLEQUIN and \
+                target_pid_1 != target_pid_2:
+            self.state.active = pid
+            self.state.step = Step.SET_TARGET
 
     @handler(Step.SET_ABILITY)
     def alchemist_ability(self, pid: PlayerID, heal: bool):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.wounded and player.role == Role.ALCHEMIST and \
+                pid == self.state.intervener:
+            target = self.state.players[self.state.target]
+            
+            if heal and len(target.shown_tokens):
+                self.state.step = Step.ALCH_HEAL
+            elif heal: # target is not wounded, cannot heal
+                self.state.active = pid
+                self.state.step = Step.SET_TARGET
+            elif len(target.shown_tokens) == 3:
+                self.state.captured = self.state.target
+                self.state.step = Step.COMPLETE
+            else:
+                self.state.wounded = self.state.target
+                self.state.next_active = pid
+                self.state.step = Step.SET_WOUND_II
+
+    @handler(Step.ALCHEMIST_HEAL)
+    def alchemist_heal(self, pid: PlayerID, token: Token):
+        player = self.state.players[pid]
+        if pid == self.state.target and token in player.shown_tokens:
+            #TODO: special case for inquisitor tokens
+            player.shown_tokens.remove(token)
+            player.tokens.append(token)
+            self.state.active = self.state.intervener
+            self.state.step = Step.SET_TARGET
 
     @handler(Step.SET_ABILITY)
     def mentalist_ability(self, pid: PlayerID, target_pid: PlayerID):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.wounded and player.role == Role.MENTALIST:
+            target = self.state.players[target_pid]
+            if len(target.shown_tokens) == 3:
+                self.state.captured = target_pid
+                self.state.step = Step.COMPLETE
+            elif Token.RANK not in target.shown_tokens:
+                self._wound_helper(target_pid, Token.RANK)
+                self.state.active = target_pid
+                self.state.step = Step.SET_TARGET
+            else:
+                self.state.wounded = target_pid
+                self.state.next_active = target_pid
+                self.state.step = Step.SET_WOUND_II
 
     @handler(Step.SET_ABILITY)
     def guardian_ability(self, pid: PlayerID, target_pid: PlayerID):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.wounded and player.role == Role.GUARDIAN:
+            target = self.state.players[target_pid]
+            # Give SWORD/SHIELD_I if nobody else has SWORD_I, else SWORD_II
+            give_sword_i = not any([Item.SWORD_I in self.state.players[p].items \
+                for p in self.player_ids and p != pid])
+            sword, shield = (Item.SWORD_I, Item.SHIELD_I) if give_sword_i else (Item.SWORD_II, Item.SHIELD_II)
+            if sword not in player.items:
+                player.items.append(sword)
+            if shield not in target.items:
+                target.items.append(shield)
+            self.state.active = pid
+            self.state.step = Step.SET_TARGET
 
     @handler(Step.SET_ABILITY)
     def berserker_ability(self, pid: PlayerID):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.wounded and player.role == Role.BERSERKER:
+            active = self.state.players[self.state.active]
+            if len(active.shown_tokens) == 3:
+                self.state.captured = self.state.active
+                self.state.step = Step.COMPLETE
+            else:
+                self.state.wounded = target_pid
+                self.state.next_active = pid
+                self.state.step = Step.SET_WOUND_II
 
     @handler(Step.SET_ABILITY)
     def mage_ability(self, pid: PlayerID, target_pid: PlayerID):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.wounded and player.role == Role.MAGE:
+            target = self.state.players[target_pid]
+            if Item.STAFF not in player.items:
+                player.items.append(Item.STAFF)
+            target.items.append(Item.STAFF)
+            self.state.active = pid
+            self.state.step = Step.SET_TARGET
 
     @handler(Step.SET_ABILITY)
     def courtesan_ability(self, pid: PlayerID, target_pid: PlayerID):
-        raise NotImplementedError
+        player = self.state.players[pid]
+        if pid == self.state.wounded and player.role == Role.COURTESAN:
+            target = self.state.players[target_pid]
+            target.items.append(Item.FAN)
+            self.state.active = pid
+            self.state.step = Step.SET_TARGET
 
+    @handler(Step.SET_WOUND_II)
+    def set_wound_ii(self, pid: PlayerID, token: Token):
+        player = self.state.players[pid]
+        if pid == self.state.wounded and self._wound_helper(pid, token):
+            self.state.active = self.state.next_active
+            self.state.step = Step.SET_TARGET
+
+    @handler(Step.COMPLETE)
+    def ack_complete(self, pid: PlayerID):
+        self.state.complete_acked[pid] = True
+        if set(self.state.complete_acked.keys()) == set(self.player_ids):
+            self.state.reset(self.player_ids)
+            self.state.step = Step.ACK_PEEK
+        
+
+if __name__ == "__main__":
+    # Mock SocketIO for unit testing
+    from json import dumps
+    class SocketIO:
+        def __init__(self):
+            self.rooms = {}
+
+        def emit(self, msg, data, json, room):
+            print(f'\n{msg}@{room}(JSON:{json}):')
+            print(dumps(data, sort_keys=True, indent=4))
+
+    def join_room(game_code: GameCode):
+        print(f'joined room {game_code}')
+
+    pids = ["aidan", "duke", "kelvin", "aaron", "piotr", "ankur", "zaaim", "annie"]
+
+    sio = SocketIO()
+    game = Game(sio, "Coven")
+    for pid in pids:
+        game.join_game(pid)
+    game.start_game('aidan')
+    for pid in pids:
+        game.ack_peek(pid)
+    import pdb
+    pdb.set_trace()
--- server/bloodbound/game_state.py
+++ server/bloodbound/game_state.py
@@ -26,6 +26,14 @@ class Token(str, Enum):
     RANK = "rank"
     TEAM = "team"
 
+    @classmethod
+    def team_sub(cls, tokens: List["Token"], team: Team):
+        def _sub(token):
+            if token == Token.TEAM:
+                return Token.RED if team == Team.RED else Token.BLUE
+            return token
+        return list(map(_sub, tokens))
+
 class Role(int, Enum):
     ELDER     = 1
     ASSASSIN  = 2
@@ -52,26 +60,29 @@ class Role(int, Enum):
         return NotImplementedError
 
 class Item(str, Enum):
-    SWORD = "sword"
     FAN = "fan"
     STAFF = "staff"
-    SHIELD = "shield"
+    SHIELD_I = "shield_i"
+    SWORD_I = "sword_i"
+    SHIELD_II = "shield_ii"
+    SWORD_II = "sword_ii"
     QUILL = "quill"
     FALSECURSE = "falsecurse"
     TRUECURSE = "truecurse"
 
 
-#TODO: populate with FSM
 class Step(str, Enum):
-    LOBBY         = "lobby"
-    SET_TARGET    = "set_target"
-    INTERVENE     = "intervene"
-    ACK_INTERVENE = "ack_intervene"
-    SET_WOUND_I   = "set_wound_i"
-    SET_ABILITY   = "set_ability"
-    SET_WOUND_II  = "set_wound_ii"
-    ACK_ABILITY   = "ack_ability"
-    COMPLETE      = "complete"
+    LOBBY          = "lobby"
+    ACK_PEEK       = "set_peek"
+    SET_TARGET     = "set_target"
+    INTERVENE      = "intervene"
+    ACK_INTERVENE  = "ack_intervene"
+    SET_WOUND_I    = "set_wound_i"
+    SET_ABILITY    = "set_ability"
+    ASSASSIN_WOUND = "assassin_wound"
+    ALCHEMIST_HEAL = "alchemist_heal"
+    SET_WOUND_II   = "set_wound_ii"
+    COMPLETE       = "complete"
 
 
 @dataclass
@@ -79,6 +90,7 @@ class Player:
     name: PlayerID
     team: Team
     role: Role
+    shown_tokens: List[Token]
     tokens: List[Token]
     items: List[Item]
     position: int
@@ -93,16 +105,27 @@ class Player:
 class GameState:
     players: Dict[PlayerID, Player] = field(default_factory=lambda: {})
     step: Step = Step.LOBBY
+    peeked: Dict[PlayerID, bool] = field(default_factory=lambda: {})
     active: Optional[PlayerID] = None
     target: Optional[PlayerID] = None
     intervene_offers: Dict[PlayerID, bool] = field(default_factory=lambda: {})
     intervener: Optional[PlayerID] = None
+    # player declaring wound token. cases:
+    #   - after attack, intervener is None ->     wounded = target
+    #   - after attack, intervener is not None -> wounded = intervener
+    #   - after assassin ability  -> wounded = ability target
+    #   - after alchemist ability -> wounded = ability target
+    #   - after mentalist ability -> wounded = ability target
+    #   - after berserker ability -> wounded = active
+    wounded: Optional[PlayerID] = None
+    # for wound_ii; alchemist & berserker keep knife, mentalist gives knife
+    next_active: Optional[PlayerID] = None
+    captured: Optional[PlayerID] = None
+    complete_acked: Dict[PlayerID, bool] = field(default_factory=lambda: {})
 
-    @classmethod
-    def build(cls, pids: Set[PlayerID], dummy=False):
+    def reset(self, pids: Set[PlayerID], dummy=False):
         if dummy:
             pids = ["aidan", "duke", "kelvin", "aaron", "piotr", "ankur", "zaaim", "annie"]
-            new_instance = GameState()
             team_size = len(pids) // 2
             red_team = ["aidan", "kelvin", "piotr", "zaaim"]
             red_roles = [Role.ALCHEMIST, Role.HARLEQUIN, Role.ASSASSIN, Role.ELDER]
@@ -110,17 +133,21 @@ class GameState:
             for (i, pid) in enumerate(pids):
                 team = Team.RED if pid in red_team else Team.BLUE
                 role = red_roles.pop() if team == Team.RED else blue_roles.pop()
-                tokens = role.tokens()
+                tokens = role.tokens(team)
                 items = []
                 player = Player(pid, team, role, tokens, items, i) # PID currently is position index
-                new_instance.players[pid] = player
-            new_instance.players["aidan"].items = [Item.SWORD, Item.STAFF]
-            new_instance.players["duke"].items = [Item.SHIELD]
-            new_instance.players["duke"].tokens = [Token.BLUE, Token.GREY]
-            new_instance.players["kelvin"].tokens = [Token.RANK]
-            return new_instance
+                self.players[pid] = player
+            self.players["aidan"].items = [Item.SWORD, Item.STAFF]
+            self.players["duke"].items = [Item.SHIELD]
+            self.players["duke"].tokens = [Token.BLUE, Token.GREY]
+            self.players["kelvin"].tokens = [Token.RANK]
+            self.peeked = {pid:True for pid in pids}
+            self.active = "ankur"
+            self.target = "aidan"
+            self.intervene_offers = {"kelvin": True}
+            self.intervener = None
+            return
         assert len(pids) % 2 == 0 # don't support inquisitor yet
-        new_instance = GameState()
         team_size = len(pids) // 2
         red_team = set(random.sample(pids, k=team_size))
         red_roles = random.sample(list(Role), k=team_size)
@@ -128,11 +155,16 @@ class GameState:
         for pid in pids:
             team = Team.RED if pid in red_team else Team.BLUE
             role = red_roles.pop() if team == Team.RED else blue_roles.pop()
-            tokens = role.tokens()
+            shown_tokens = []
+            tokens = Token.team_sub(role.tokens(), team)
             items = []
-            player = Player(pid, team, role, tokens, items, 0) # TODO make position correct
-            new_instance.players[pid] = player
-        return new_instance
+            player = Player(pid, team, role, shown_tokens, tokens, items, 0) # TODO make position correct
+            self.players[pid] = player
+            self.peeked = {}
+            self.active = None
+            self.target = None
+            self.intervene_offers = {}
+            self.intervener = None
 
     def encode(self) -> Dict[str, Any]:
         return asdict(self)
